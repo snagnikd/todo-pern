920b24d62c415de1b5af1d815db9d76a892c5bd8
Snagnik <snagnikd@gmail.com>
2024-03-30 15:03:32 +0530

    delete and input implemented

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
+
 function App() {
-  return <Fragment></Fragment>;
+  return (
+  <Fragment>
+    <div className='container'>
+      <InputTodo />
+      <ListTodos />
+    </div>
+  </Fragment>
+  );
 }
 
 export default App;
---
+++ src/components/ListTodos.js
@@ -0,0 +1,55 @@
+import React, {Fragment, useEffect, useState} from "react";
+
+const ListTodos = () => {
+    const [todos, setTodos] = useState([]);
+
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: 'DELETE'
+            });
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    const getTodos = async () => {
+        try {
+            const response = await fetch('http://localhost:5000/todos');
+            const jsonData = await response.json();
+
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return (
+    <Fragment>
+        <table className="table mt-5 text-center">
+            <thead>
+            <tr>
+                <th>Description</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            </thead>
+            <tbody>
+                {todos.map(todo => (
+                    <tr key={todo.todo_id}>
+                        <td>{todo.description}</td>
+                        <td>Edit</td>
+                        <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </Fragment>
+    );
+};
+
+export default ListTodos;
